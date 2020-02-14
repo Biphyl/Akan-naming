cf9a2c9fda742f49fb95cb841b8fe3e2a8cdd4d2
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-02-14 18:07:32 +0300

    created arrays

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ function myFunction(){
   var DD = parseInt(day);
   var CC = (YY-1)/100+1;
   var cent = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
-
+  var boy = ["Kwasi",,"Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var girl = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  
+  
 }
   
