a7af056f084de72f70d7d90f7263e4abf98ca4d4
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-02-14 18:06:16 +0300

    created variables

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function myFunction(){
- 
+  var year = document.getElementById("year").nodeValue;
+  var YY = parseInt(year);
+  var month = document.getElementById("month").nodeValue;
+  var MM = parseInt(month);
+  var day = document.getElementById("day").nodeValue;
+  var DD = parseInt(day);
+  var CC = (YY-1)/100+1;
+  var cent = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+
 }
   
